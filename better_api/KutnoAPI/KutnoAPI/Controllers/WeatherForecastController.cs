5d636e1d0055c53b99ed887532d27508672462ae
Andrzej Niedziółka <niedziolkandrzej@gmail.com>
2023-09-30 22:39:39 +0200

    addded docker

--- better_api/KutnoAPI/KutnoAPI/Controllers/WeatherForecastController.cs
+++ better_api/KutnoAPI/KutnoAPI/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using KutnoAPI.Parsers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KutnoAPI.Controllers
@@ -6,28 +7,19 @@ namespace KutnoAPI.Controllers
 	[Route("[controller]")]
 	public class WeatherForecastController : ControllerBase
 	{
-		private static readonly string[] Summaries = new[]
-		{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
-
 		private readonly ILogger<WeatherForecastController> _logger;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration con)
 		{
 			_logger = logger;
 		}
 
-		[HttpGet(Name = "GetWeatherForecast")]
-		public IEnumerable<WeatherForecast> Get()
+		[HttpGet]
+		public async Task<ObjectResult> Get()
 		{
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-			{
-				Date = DateTime.Now.AddDays(index),
-				TemperatureC = Random.Shared.Next(-20, 55),
-				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-			})
-			.ToArray();
+
+			return Ok("Zajebiœcie");
+
 		}
 	}
 }
---
+++ better_api/KutnoAPI/KutnoAPI/Parsers/SchoolParser.cs
@@ -0,0 +1,58 @@
+﻿using KutnoAPI.Models;
+using System.Xml.Linq;
+
+namespace KutnoAPI.Parsers;
+
+public class SchoolParser
+{
+	private readonly XMLParser xMLParser;
+
+	public SchoolParser()
+	{
+		xMLParser = new XMLParser();
+	}
+
+	//public void Parse(byte[] bytes)
+	//{
+	//	string fileName = "C:\\Users\\ANI\\source\\repos\\HackYeah\\docs\\SIO 30.09.2022.xml";
+	//	string sheetName = "Szkoły i placówki";
+
+	//	var result = xMLParser.ParseXmlToDataTable(fileName, sheetName);
+	//}
+
+	public List<School> Parse(byte[] bytes)
+	{
+		string fileName = "C:\\Users\\ANI\\source\\repos\\HackYeah\\docs\\SIO 30.09.2022.xml";
+		bytes = File.ReadAllBytes(fileName);
+		// Create an XDocument from the byte array
+		using MemoryStream stream = new(bytes);
+
+		XDocument xmlDoc = XDocument.Load(stream);
+
+		// Specify the sheet name
+		string sheetName = "Szkoły i placówki";
+
+		// Extract data from the XML
+		var schools = xmlDoc
+			.Descendants("Worksheet")
+			.FirstOrDefault(ws => ws.Attribute("Name")?.Value == sheetName)
+			?.Descendants("Row")
+			.Skip(5) // Skip header rows
+			.Select(row => new School
+			{
+				Rspo = Convert.ToInt16(row.Elements("Cell").ElementAt(0).Value),
+				Regon = row.Elements("Cell").ElementAt(1).Value,
+				SchoolType = Convert.ToInt32(row.Elements("Cell").ElementAt(2).Value),
+				Name = row.Elements("Cell").ElementAt(3).Value,
+				Address = row.Elements("Cell").ElementAt(4).Value,
+				BuildingNumber = row.Elements("Cell").ElementAt(5).Value,
+				FlatNumber = row.Elements("Cell").ElementAt(6).Value,
+				Town = row.Elements("Cell").ElementAt(7).Value,
+				PostCode = row.Elements("Cell").ElementAt(8).Value,
+				Post = row.Elements("Cell").ElementAt(9).Value
+			})
+			.ToList();
+
+		return schools;
+	}
+}
