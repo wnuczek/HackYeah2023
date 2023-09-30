395daf56206ec99b1940f3f8fd7571d5797956e5
janek <jan.nagadowski@reesco.pl>
2023-09-30 19:55:43 +0200

    added top bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { RaportComponent } from './raport/raport.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
+import { MenubarModule } from 'primeng/menubar';
 
 const primeng = [AccordionModule];
 @NgModule({
@@ -20,6 +21,7 @@ const primeng = [AccordionModule];
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MenubarModule,
     ...primeng,
   ],
   providers: [],
