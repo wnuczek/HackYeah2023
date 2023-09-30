2003ffec68cf8f8e3629d39fc46e391f536c2473
PGJ <gajewski.pawel@sciteex.com>
2023-09-30 23:10:33 +0200

    Add upload api

--- better_api/KutnoAPI/KutnoAPI/Models/SchoolSummary.cs
+++ better_api/KutnoAPI/KutnoAPI/Models/SchoolSummary.cs
@@ -6,10 +6,20 @@ namespace KutnoAPI.Models
     {
         [JsonPropertyName("schoolrspo")]
         public long SchoolRSPO { get; set; }
+
+        [JsonPropertyName("year")]
         public int Year { get; set; }
+
+        [JsonPropertyName("studentsquantity")]
         public decimal StudentsQuantity { get; set; }
+
+        [JsonPropertyName("studentsfromcountryquantity")]
         public decimal StudentsFromCountryQuantity { get; set; }
+
+        [JsonPropertyName("studentsfromsmalltownquantity")]
         public decimal StudentsFromSmallTownQuantity { get; set; }
+
+        [JsonPropertyName("studentsqutsideschool")]
         public decimal StudentsOutsideSchool { get; set; }
     }
 }
