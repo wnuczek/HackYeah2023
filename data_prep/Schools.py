fdb6e316655887f6b9a0e54b75039ccb116765e6
PGJ <gajewski.pawel@sciteex.com>
2023-10-01 07:15:46 +0200

    Add Schools to dapaprep

---
+++ data_prep/Schools.py
@@ -0,0 +1,19 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import pandas as pd
+
+# Disable SSL certificate verification warning (not recommended for production)
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+url = "http://localhost:5286/Schools"
+
+payload = """{
+}"""
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+df = pd.read_json(response.text)
+print(df)
